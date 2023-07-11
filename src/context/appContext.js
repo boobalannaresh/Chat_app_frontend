4d8f4a37d0f886020d1dad49b1d8defdc0a90cc2
boobalannaresh <bestforevermvi@gmail.com>
2023-07-11 19:38:33 +0530

    socket does not connect

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -2,7 +2,19 @@ import { io } from "socket.io-client";
 import React from "react";
 
 
-const SOCKET_URL ="https://chat-app-backend-chi-virid.vercel.app";
-export const socket = io(SOCKET_URL);
+// const SOCKET_URL ="https://chat-app-backend-chi-virid.vercel.app";
+// export const socket = io(SOCKET_URL);
+
+
+var connectionOptions = {
+    "force new connection": true,
+    reconnectionAttempts: "Infinity",
+    timeout: 10000,
+    transports: ["websocket"],
+  };
+  const ENDPOINT = "https://chat-app-backend-chi-virid.vercel.app"; 
+export  const socket = io(ENDPOINT, connectionOptions);
+
+
 // app context
 export const AppContext = React.createContext();
